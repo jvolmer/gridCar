e29a8762c3eb2ccf7c6885a74bfbbddfb2521e70
jvolmer <juliavolmer@t-online.de>
2020-08-31 23:05:44 +0200

    Modify main to test messaging

--- cocktailMixer.ino
+++ cocktailMixer.ino
@@ -1,4 +1,6 @@
 #include "src/communication/arduinoTransmitter.hpp"
+#include "src/communication/messageToCar.hpp"
+#include "src/communication/messageFromCar.hpp"
 #include "src/movement/position/coordinate.hpp"
 #include <SPI.h>
 #include <RF24.h>
@@ -7,8 +9,11 @@ RF24 radio{9, 10};
 const unsigned char address[5] = {'C', 'a', 'r', '0', '1'};
 ArduinoTransmitter transmitter{ address, radio };
 
-Coordinate coordinate{ 1, 0 };
-Coordinate received{0, 0};
+Coordinate send{ 1, 0 };
+Coordinate received{ 0, 0 };
+
+MessageFromCar messageOut{ transmitter };
+MessageToCar messageIn{ transmitter };
 
 void setup() {
     Serial.begin(9600);
@@ -16,11 +21,13 @@ void setup() {
 }
 
 void loop() {
-    transmitter.setReply(coordinate);
-    transmitter.replyToReception(received);
-    Serial.print(coordinate.getx());
+    messageOut.set( send );
+    messageOut.supplyForNextReception();
+    received = messageIn.receive();
+    
+    Serial.print(received.getx());
     Serial.print(", ");
-    Serial.print(coordinate.gety());
+    Serial.print(received.gety());
     Serial.println();
 
 }
